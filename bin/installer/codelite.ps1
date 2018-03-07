4add22cbec8fdcc1f487be480a0af20e60cf9f57
unknown <DasBaumwolltier>
2018-03-07 11:12:04 +0100

    codelite

---
+++ bin/installer/codelite.ps1
@@ -0,0 +1,39 @@
+Param($path, $installer, $version)
+
+# $tmpPath = "$path\_scoop_extract"
+
+# mkdir $tmpPath -ErrorAction SilentlyContinue
+
+# Write-Host "- 1/2 Extracting codelite 7zip archive..."
+
+# 7z x -o"$tmpPath" $path\$installer > $null
+
+# if($LASTEXITCODE -ne 0) 
+# { 
+#     exit "Exit code was $LASTEXITCODE" 
+# }
+
+# Write-Host "  done."
+
+Write-Host "- Extracting codelite innosetup..."
+
+Move-Item "$tmpPath\codelite*.exe" "$tmpPath\codelite.exe"
+innounp -x -d"$path\_scoop_unpack" "$tmpPath\codelite.exe" > "$path\innounp.log"
+
+if($LASTEXITCODE -ne 0)
+{
+    exit "Exit code was $LASTEXITCODE"
+}
+
+Get-ChildItem "$path\_scoop_unpack\{app}" -r | Move-Item -dest "$path" -force
+
+Remove-Item -r -force "$path\_scoop_unpack"
+# Remove-Item -r -force "$path\_scoop_extract"
+Remove-Item -r -force "$path\innounp.log"
+
+Remove-Item "$path\$installer"
+Write-Host "  done."
+
+# Remove-Item $path\codelite.7z
+# Move-Item $path\codelite*.exe $path\codelite.exe
+
