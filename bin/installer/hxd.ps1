de3fc81bc144a400dd5504b67e4c2941352d3835
DasBaumwolltier <gabriel@guldner.eu>
2018-10-14 15:43:19 +0200

    hxd fixed

--- bin/installer/hxd.ps1
+++ bin/installer/hxd.ps1
@@ -1,6 +1,6 @@
 Param($path, $installer, $version)
 
-"$path\_scoop_unpack\HxDSetup.exe" /VERYSILENT /NOICONS /NORESTART /SUPPRESSMSGBOXES /TYPE=portable /DIR=$path
+& "$path\_scoop_unpack\HxDSetup.exe" "/VERYSILENT /NOICONS /NORESTART /SUPPRESSMSGBOXES /TYPE=portable /DIR=$path"
 
 if($LASTEXITCODE -ne 0)
 {
